f0d66a0391524886b817f782db3a819b2355e6bf
Abhinand SD <abhinandssd90@gmail.com>
2025-09-24 09:47:58 +0530

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slidebar from './components/Slidebar'
+import Dashboard from './components/Dashboard'
+
+const App = () => {
+  return (
+    <div className='flex'>
+    <Slidebar />
+    <Dashboard />
+      
+    </div>
+  )
+}
+
+export default App
