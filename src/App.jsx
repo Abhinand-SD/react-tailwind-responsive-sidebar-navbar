f33ba718876f6659f527d7d10681b1153bbbe9e3
Abhinand SD <abhinandssd90@gmail.com>
2025-09-25 13:16:42 +0530

    completed commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slidebar from './components/Slidebar'
 import Dashboard from './components/Dashboard'
 
 const App = () => {
+  const [sidebarToggle, setSidebarToggle] = useState(true)
   return (
     <div className='flex'>
-    <Slidebar />
-    <Dashboard />
-      
+      <Slidebar sidebarToggle={sidebarToggle}/>
+      <Dashboard setSidebarToggle={setSidebarToggle} sidebarToggle={sidebarToggle}/>
     </div>
   )
 }
